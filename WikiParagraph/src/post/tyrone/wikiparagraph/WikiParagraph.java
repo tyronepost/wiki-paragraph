6f5bb221a9cfab2905be52983f19e8d43968fe2b
Tyrone Post <tyrone.post.a@gmail.com>
2016-01-21 18:21:24 -0500

    New test case.  Fixed getParagraph() method to use topic parameter.

--- WikiParagraph/src/post/tyrone/wikiparagraph/WikiParagraph.java
+++ WikiParagraph/src/post/tyrone/wikiparagraph/WikiParagraph.java
@@ -10,7 +10,7 @@ import org.jsoup.select.Elements;
 public class WikiParagraph {
 	
 	public String getParagraph(String topic) throws IOException {
-		Document doc = Jsoup.connect("https://en.wikipedia.org/wiki/Dog").get();
+		Document doc = Jsoup.connect("https://en.wikipedia.org/wiki/" + topic).get();
 		Element element = doc.getElementById("mw-content-text");
 		Elements children = element.children().not("div");
 		Element child = children.first();
--- WikiParagraphTest/src/post/tyrone/wikiparagraph/WikiParagraphTest.java
+++ WikiParagraphTest/src/post/tyrone/wikiparagraph/WikiParagraphTest.java
@@ -14,8 +14,28 @@ public class WikiParagraphTest {
 		String expected = "The domestic dog (Canis lupus familiaris or Canis familiaris) "
 				+ "is a domesticated canid which has been selectively bred for millennia for "
 				+ "various behaviors, sensory capabilities, and physical attributes.[2]";
+		
 		WikiParagraph wikiParagraph = new WikiParagraph();
-		String actual = wikiParagraph.getParagraph("dog");
+		String actual = wikiParagraph.getParagraph("dog");	
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testTopic_Cat() throws IOException {
+		String expected = "The domestic cat[1][2] (Felis catus or Felis silvestris catus)[2]"
+				+ "[4] is a small, typically furry, domesticated, and carnivorous mammal. They "
+				+ "are often called house cats when kept as indoor pets or simply cats when "
+				+ "there is no need to distinguish them from other felids and felines.[6] "
+				+ "Cats are often valued by humans for companionship and for their ability to "
+				+ "hunt vermin. There are more than 70 cat breeds; different associations "
+				+ "proclaim different numbers according to their standards. The International "
+				+ "Progressive Cat Breeders Alliance (IPCBA) recognizes 73 cat breeds while "
+				+ "TICA (The International Cat Association) recognizes 58, CFA (The Cat Fanciers' "
+				+ "Association) recognizes 44 and Federation International Feline (FIFE) "
+				+ "recognizes 43.[7]";
+		
+		WikiParagraph wikiParagraph = new WikiParagraph();
+		String actual = wikiParagraph.getParagraph("cat");	
 		assertEquals(expected, actual);
 	}
 }
