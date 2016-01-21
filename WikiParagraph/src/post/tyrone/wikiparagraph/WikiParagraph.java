7d084924fef366a743308d95caabce7fed1ae094
Tyrone Post <tyrone.post.a@gmail.com>
2016-01-21 18:43:55 -0500

    Black_Hole page had a <table> element preceding the first paragraph. Added "table" to Element filter.  Extracted filterElements() private method.

--- WikiParagraph/src/post/tyrone/wikiparagraph/WikiParagraph.java
+++ WikiParagraph/src/post/tyrone/wikiparagraph/WikiParagraph.java
@@ -12,8 +12,13 @@ public class WikiParagraph {
 	public String getParagraph(String topic) throws IOException {
 		Document doc = Jsoup.connect("https://en.wikipedia.org/wiki/" + topic).get();
 		Element element = doc.getElementById("mw-content-text");
-		Elements children = element.children().not("div");
+		Elements children = filterElements(element);
 		Element child = children.first();
 		return child.text();
+	}
+
+	private Elements filterElements(Element element) {
+		Elements children = element.children().not("div").not("table");
+		return children;
 	}
 }
