3322044fe06420cc1df8036647927f1eb62fd893
Tyrone Post <tyrone.post.a@gmail.com>
2016-01-21 18:12:42 -0500

    first passing unit test for returning paragraph

--- WikiParagraph/src/post/tyrone/wikiparagraph/WikiParagraph.java
+++ WikiParagraph/src/post/tyrone/wikiparagraph/WikiParagraph.java
@@ -1,5 +1,19 @@
 package post.tyrone.wikiparagraph;
 
-public class WikiParagraph {
+import java.io.IOException;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class WikiParagraph {
+	
+	public String getParagraph(String topic) throws IOException {
+		Document doc = Jsoup.connect("https://en.wikipedia.org/wiki/Dog").get();
+		Element element = doc.getElementById("mw-content-text");
+		Elements children = element.children().not("div");
+		Element child = children.first();
+		return child.text();
+	}
 }
--- WikiParagraphTest/src/post/tyrone/wikiparagraph/WikiParagraphTest.java
+++ WikiParagraphTest/src/post/tyrone/wikiparagraph/WikiParagraphTest.java
@@ -2,13 +2,20 @@ package post.tyrone.wikiparagraph;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
 public class WikiParagraphTest {
 
 	@Test
-	public void test() {
-		WikiParagraph wikiParagraph = new WikiParagraph();
-	}
+	public void testTopic_Dog() throws IOException {
 
+		String expected = "The domestic dog (Canis lupus familiaris or Canis familiaris) "
+				+ "is a domesticated canid which has been selectively bred for millennia for "
+				+ "various behaviors, sensory capabilities, and physical attributes.[2]";
+		WikiParagraph wikiParagraph = new WikiParagraph();
+		String actual = wikiParagraph.getParagraph("dog");
+		assertEquals(expected, actual);
+	}
 }
