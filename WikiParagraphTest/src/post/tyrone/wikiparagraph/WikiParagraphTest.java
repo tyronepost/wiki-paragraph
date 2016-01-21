8be9b04503003563a674da9e1df1e4f4df8a8775
Tyrone Post <tyrone.post.a@gmail.com>
2016-01-21 18:35:31 -0500

    black hole test breaks.  returns something completely unexpected.

--- WikiParagraphTest/src/post/tyrone/wikiparagraph/WikiParagraphTest.java
+++ WikiParagraphTest/src/post/tyrone/wikiparagraph/WikiParagraphTest.java
@@ -4,18 +4,24 @@ import static org.junit.Assert.*;
 
 import java.io.IOException;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class WikiParagraphTest {
 
+	WikiParagraph wikiParagraph;
+	
+	@Before
+	public void setUp() {
+		wikiParagraph = new WikiParagraph();	
+	}
+	
 	@Test
 	public void testTopic_Dog() throws IOException {
-
 		String expected = "The domestic dog (Canis lupus familiaris or Canis familiaris) "
 				+ "is a domesticated canid which has been selectively bred for millennia for "
 				+ "various behaviors, sensory capabilities, and physical attributes.[2]";
 		
-		WikiParagraph wikiParagraph = new WikiParagraph();
 		String actual = wikiParagraph.getParagraph("dog");	
 		assertEquals(expected, actual);
 	}
@@ -34,8 +40,26 @@ public class WikiParagraphTest {
 				+ "Association) recognizes 44 and Federation International Feline (FIFE) "
 				+ "recognizes 43.[7]";
 		
-		WikiParagraph wikiParagraph = new WikiParagraph();
 		String actual = wikiParagraph.getParagraph("cat");	
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testTopic_BlackHole() throws IOException {
+		String expected = "A black hole is a geometrically defined region of spacetime exhibiting "
+				+ "such strong gravitational effects that nothing—including particles and "
+				+ "electromagnetic radiation such as light—can escape from inside it.[1] The theory "
+				+ "of general relativity predicts that a sufficiently compact mass can deform spacetime "
+				+ "to form a black hole.[2][3] The boundary of the region from which no escape is possible "
+				+ "is called the event horizon. Although crossing the event horizon has enormous effect on "
+				+ "the fate of the object crossing it, it appears to have no locally detectable features. "
+				+ "In many ways a black hole acts like an ideal black body, as it reflects no light.[4][5] "
+				+ "Moreover, quantum field theory in curved spacetime predicts that event horizons emit "
+				+ "Hawking radiation, with the same spectrum as a black body of a temperature inversely "
+				+ "proportional to its mass. This temperature is on the order of billionths of a kelvin "
+				+ "for black holes of stellar mass, making it essentially impossible to observe.";
+		
+		String actual = wikiParagraph.getParagraph("Black_hole");	
 		assertEquals(expected, actual);
 	}
 }
