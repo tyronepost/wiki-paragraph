c025a68ffa50305567bafc25f41bb6d5b6f5c69f
Tyrone Post <tyrone.post.a@gmail.com>
2016-01-21 20:09:28 -0500

    second pass at cli, with single arg

--- WikiParagraph/src/post/tyrone/app/Application.java
+++ WikiParagraph/src/post/tyrone/app/Application.java
@@ -11,14 +11,28 @@ public class Application {
 
 	public static void main(String[] args) {
 
-		try (Scanner scanner = new Scanner(System.in)){
+		String input;
+		if (args.length > 0) {
+			input = args[0];
 			WikiParagraph wikiParagraph = new WikiParagraph();
-			out.print("Enter a topic: ");
-			String input = scanner.nextLine();
-			String paragraph = wikiParagraph.getParagraph(input);
-			out.println(paragraph);
-		} catch (WikiParagraphException e) {
-			out.print("NOT FOUND");
+			String paragraph;
+			try {
+				paragraph = wikiParagraph.getParagraph(input);
+				out.println(paragraph);
+			} catch (WikiParagraphException e) {
+				out.print("NOT FOUND");
+			}
+
+		} else {
+			try (Scanner scanner = new Scanner(System.in)) {
+				WikiParagraph wikiParagraph = new WikiParagraph();
+				out.print("Enter a topic: ");
+				input = scanner.nextLine();
+				String paragraph = wikiParagraph.getParagraph(input);
+				out.println(paragraph);
+			} catch (WikiParagraphException e) {
+				out.print("NOT FOUND");
+			}
 		}
 	}
 }
