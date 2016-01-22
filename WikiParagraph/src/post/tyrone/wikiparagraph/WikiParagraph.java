0ae7a045b2b63cba1af8df753c88d309439e521d
Tyrone Post <tyrone.post.a@gmail.com>
2016-01-21 21:45:22 -0500

    light refactoring

--- WikiParagraph/src/post/tyrone/wikiparagraph/WikiParagraph.java
+++ WikiParagraph/src/post/tyrone/wikiparagraph/WikiParagraph.java
@@ -11,6 +11,7 @@ import org.jsoup.select.Elements;
 public class WikiParagraph {
 	
 	private final String CONTAINING_ELEMENT = "mw-content-text"; 
+	
 	public String getParagraph(String topic) throws WikiParagraphException {
 		Document doc = getDocument(topic);
 		Element element = doc.getElementById(CONTAINING_ELEMENT);
@@ -20,8 +21,7 @@ public class WikiParagraph {
 
 	private Document getDocument(String topic) throws WikiParagraphException {
 		try {
-			Document doc = Jsoup.connect("https://en.wikipedia.org/wiki/" + topic).get();
-			return doc;
+			return Jsoup.connect("https://en.wikipedia.org/wiki/" + topic).get();
 		} catch (IOException e) {
 			throw new WikiParagraphException("Failed to retrieve document topic", e);
 		}
